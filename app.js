35859799e8140e2e501df210ecf6df194186f6db
akhilkhethavath <161169235+akhilkhethavath@users.noreply.github.com>
2024-10-11 00:23:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'https://xenon-frontend-omega.vercel.app/',
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE',
     credentials:true,
 }));
